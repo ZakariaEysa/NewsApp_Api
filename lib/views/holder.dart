13a585eec106898f8d12e3fa2e0e8aa757cbe3c7
Zakaria Eysa <zakariaeysa@gmail.com>
2024-05-26 09:49:17 +0300

    News App done !

--- lib/views/holder.dart
+++ lib/views/holder.dart
@@ -5,7 +5,8 @@ import 'package:news_app_ui_setup/serveses/newsServes.dart';
 import 'package:news_app_ui_setup/views/news_tile.dart';
 
 class NewsListView extends StatefulWidget {
-  const NewsListView( required List<ArticleModel> listy{
+  const NewsListView( {
+
   super.key, required List<ArticleModel> listy,
   });
 
@@ -23,7 +24,7 @@ class _NewsListViewState extends State<NewsListView> {
   }
 
   Future<void> getnewsGeneral() async {
-    listy = await newServes(Dio()).getNews();
+    listy = await newServes(Dio()).getNews(Ccategory: '');
     isloading = false;
     setState(() {});
   }
